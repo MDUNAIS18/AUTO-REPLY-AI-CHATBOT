a90411b0e99cb56684673b52c21523eee1c98bfd
UNAIS <mdmaddy345@gmail.com>
2025-03-14 16:33:13 +0530

    Update openai_script.py

--- openai_script.py
+++ openai_script.py
@@ -4,20 +4,14 @@ from openai import OpenAI
 
 # Initialize the OpenAI client with the API key
 client = OpenAI(
-    api_key= "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"
+    api_key= # add your api key
 ) 
 
 # Your conversation or command content
-command = ''' 
-Copied text: [11:06 pm, 10/3/2025] Arya: Unais eapude irukan nu kekura 
-[11:06 pm, 10/3/2025] Arya: Ena solatum 
-[11:07 pm, 10/3/2025] MD UNAIS: Unaku thariyadha msg pannu 
-[11:07 pm, 10/3/2025] MD UNAIS: Thariyadha maari kekuriyaa 
-[11:07 pm, 10/3/2025] Arya: Theriyatha nala dha kekura 
-[11:08 pm, 10/3/2025] MD UNAIS: Daii daiii summa thariyadha maari nadikuradha enta 
-[11:09 pm, 10/3/2025] Arya: Unmaya unais 
-[11:09 pm, 10/3/2025] MD UNAIS: nee pesu first ava ready pannu
-'''
+command =  
+
+# there add the command like the message form the terminal output use the ''' ''' for ckeck the command is for the sender. 
+
 
 # Calling OpenAI API to generate a response
 completion = client.chat.completions.create(
